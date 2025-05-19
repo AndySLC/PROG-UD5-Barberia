2af6fa2007b6ecc68d75a8ea3facc5f97ace5aaa
andyogb <usr3962@salesianos-lacuesta.net>
2025-05-19 17:52:28 +0100

    feat:objetoBarberia

--- 1.0/src/net/salesianos/Barberia.java
+++ 1.0/src/net/salesianos/Barberia.java
@@ -1,5 +1,39 @@
 package net.salesianos;
 
 public class Barberia {
-    
+    private String nombre;
+    private String ubicacion;
+    private String barberos;
+    private int puntuacion; 
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getUbicacion() {
+        return ubicacion;
+    }
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    public String getBarberos() {
+        return barberos;
+    }
+    public void setBarberos(String barberos) {
+        this.barberos = barberos;
+    }
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    @Override
+    public String toString() {
+        return "Barberia [nombre=" + nombre + ", ubicacion=" + ubicacion + ", barberos=" + barberos + ", puntuacion="
+                + puntuacion + "]";
+    }
 }
+
